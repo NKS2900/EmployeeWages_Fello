2ea632d07a78aeb7f0380160cea41dd16a116ca6
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-11-12 10:21:27 +0530

    Added: PartTime wages Using SwitchCase

--- EmpChek.java
+++ EmpChek.java
@@ -13,29 +13,35 @@ public class EmpChek {
 
 	static void PartTime() {
 		Scanner in = new Scanner(System.in);
-		int isFullTime = 1;
-		int isPartTime = 0;
+
 		int perHour = 20;
 		int FullDayHour = 8;
+		int day;
+
 		Random rn = new Random();
 		int rand = rn.nextInt(3);
 
-		if (rand == isPartTime) {
-			System.out.println("-------PartTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			int day = in.nextInt();
-			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
+		switch (rand) {
 
-		} else if (rand == isFullTime) {
+		case 1:
 			System.out.println("-------FullTime_Employee_Wages-------");
 			System.out.println("Enter no Days Employee Worked : ");
-			int day = in.nextInt();
+			day = in.nextInt();
 			int FullTimeWages = perHour * (FullDayHour * day);
-			System.out.println("FullTime_Wages_for " + day + " days : " + FullTimeWages + " Rs.");
-		} else
-			System.out.println("Employee Absent..!!!!");
+			System.out.println("Full_Time_Wages_for " + day + " days : S" + FullTimeWages + " Rs.");
+			break;
+		case 2:
+			System.out.println("-------PartTime_Employee_Wages-------");
+			System.out.println("Enter no Days Employee Worked : ");
+			day = in.nextInt();
+			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
+			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
+			break;
+		default:
+			System.out.println("Employee is Absent..!!!!");
+			break;
 
+		}
 		in.close();
 	}
 }
