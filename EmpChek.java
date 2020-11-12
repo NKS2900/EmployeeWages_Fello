e71f77d878843de789915b0f38a022ba5bcdc061
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-11-12 10:33:22 +0530

    Added: Employee wages Till_Condition

--- EmpChek.java
+++ EmpChek.java
@@ -5,31 +5,62 @@ import java.util.Scanner;
 
 public class EmpChek {
 
-	public static void main(String x[]) {
-		System.out.println("-------Employee_Wages--------");
+	public static final int FullDayHour = 8;
+	public static final int WorkingDayMonth = 20;
+	public static final int maxHoursInMonth = 100;
+	public static final int perHour = 20;
 
+	public static void main(String x[]) {
+
+		//UC6_Employee_Wages _Till The Condition
 		MonthlyWages();
 	}
 
 	static void MonthlyWages() {
-		int perHour = 20;
-		int FullDayHour = 8;
-		int WorkingDayMonth = 20;
+		int fullTimeDays = 0;
+		int partTimeDays = 0;
+		int totalempHour = 0;
+		int totalWorkingDay = 0;
+		int absent = 0;
 
-		Random rn = new Random();
-		int rand = rn.nextInt(2);
+		while (maxHoursInMonth > totalempHour && WorkingDayMonth > totalWorkingDay) {
 
-		if(rand==0) {
-			System.out.println("-------FullTime_Employee_Wages_Monthly-------");
-			int FullTimeWages = perHour * (FullDayHour * WorkingDayMonth);
-			System.out.println("Full_Time_Wages_for " + WorkingDayMonth + " days : " + FullTimeWages + " Rs.");
+			totalWorkingDay++;
+
+			Random rn = new Random();
+			int rand = rn.nextInt(3);
+
+			switch (rand) {
+
+			case 0:
+				fullTimeDays++;
+				totalempHour = totalempHour + FullDayHour;
+				break;
+			case 1:
+				partTimeDays++;
+				totalempHour = totalempHour + (FullDayHour / 2);
+				break;
+			default:
+				absent++;
+				break;
+				
+			}
 		}
-		else {
-			System.out.println("-------PartTime_Employee_Wages_Monthly-------");
-			int partTimeWages = (WorkingDayMonth * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + WorkingDayMonth + " days : " + partTimeWages + " Rs.");
-		}
+		int FullTimeSalary = perHour * (FullDayHour * fullTimeDays);
+		int PartTimeSalary = (partTimeDays * (FullDayHour / 2)) * perHour;
+
+		System.out.println("------------Employee_Wages-------------");
+		System.out.println("---------------------------------------");
+		System.out.println("FullTime_Days_in_month: " + fullTimeDays);
+		System.out.println("PartTime_Days_in_month: " + partTimeDays);
+		System.out.println("Absent_Days_in_month: " + absent);
+		System.out.println("Total_working_Days_IN_month : " + (fullTimeDays + partTimeDays));
+		System.out.println("Total_Hours    :===> " + totalempHour);
+		System.out.println("FullTime_Salary:===> " + FullTimeSalary);
+		System.out.println("PartTime_Salary:===> " + PartTimeSalary);
+		System.out.println("Total_Salary   :===> " + (FullTimeSalary + PartTimeSalary));
+		System.out.println("---------------------------------------");
+		System.out.println(totalWorkingDay);
 
 	}
-
 }
