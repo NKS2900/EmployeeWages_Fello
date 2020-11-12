7db26d779b8cec4c984fbaaa984169d9420725fd
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-11-12 10:27:33 +0530

    Added: Employee Monthly wages

--- EmpChek.java
+++ EmpChek.java
@@ -6,42 +6,30 @@ import java.util.Scanner;
 public class EmpChek {
 
 	public static void main(String x[]) {
-		System.out.println("******Employee_Daily_Wages*******");
+		System.out.println("-------Employee_Wages--------");
 
-		PartTime();
+		MonthlyWages();
 	}
 
-	static void PartTime() {
-		Scanner in = new Scanner(System.in);
-
+	static void MonthlyWages() {
 		int perHour = 20;
 		int FullDayHour = 8;
-		int day;
+		int WorkingDayMonth = 20;
 
 		Random rn = new Random();
-		int rand = rn.nextInt(3);
+		int rand = rn.nextInt(2);
 
-		switch (rand) {
-
-		case 1:
-			System.out.println("-------FullTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			day = in.nextInt();
-			int FullTimeWages = perHour * (FullDayHour * day);
-			System.out.println("Full_Time_Wages_for " + day + " days : S" + FullTimeWages + " Rs.");
-			break;
-		case 2:
-			System.out.println("-------PartTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			day = in.nextInt();
-			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
-			break;
-		default:
-			System.out.println("Employee is Absent..!!!!");
-			break;
-
+		if(rand==0) {
+			System.out.println("-------FullTime_Employee_Wages_Monthly-------");
+			int FullTimeWages = perHour * (FullDayHour * WorkingDayMonth);
+			System.out.println("Full_Time_Wages_for " + WorkingDayMonth + " days : " + FullTimeWages + " Rs.");
 		}
-		in.close();
+		else {
+			System.out.println("-------PartTime_Employee_Wages_Monthly-------");
+			int partTimeWages = (WorkingDayMonth * (FullDayHour / 2)) * perHour;
+			System.out.println("PartTime_Wages_for " + WorkingDayMonth + " days : " + partTimeWages + " Rs.");
+		}
+
 	}
+
 }
